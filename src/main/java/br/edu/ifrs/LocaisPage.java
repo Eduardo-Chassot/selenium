61cdba66f89c2ee84d61c83ddd43907176d845db
NIE9 <o.thiagonienov@gmail.com>
2025-07-07 18:12:13 -0300

    Locais
    Implemented various helper methods in LocaisPage for interacting with the UI, including actions for creating, editing, filtering, and deleting 'Local' entries. Updated TesteLocais to use JUnit parameterized tests, added test cases for creating, validating, editing, and deleting locais, and integrated Faker for dynamic test data generation.

--- src/main/java/br/edu/ifrs/LocaisPage.java
+++ src/main/java/br/edu/ifrs/LocaisPage.java
@@ -1,5 +1,104 @@
 package br.edu.ifrs;
 
+import org.openqa.selenium.By;
+
 public class LocaisPage {
-    
+    								//html/body/app-root/app-container/main/div/app-local/p-toast/div/p-toastitem/div/div/div/div[1]
+	private String pathMensagem = "/html/body/app-root/app-container/main/div/app-local/p-toast/div/p-toastitem/div/div/div/div[2]";
+	private String pathMensagemEditadoSucesso = "/html/body/app-root/app-container/main/div/app-local/p-toast/div/p-toastitem";
+	private String msgSucesso = "Local cadastrado com sucesso!";
+	
+	private String msgDescricaoDuplicada = "Já existe um Local com o mesmo nome!";
+	private String msgEditadoSucesso = "Local editado com sucesso!";
+	public String msgDeleteSucesso = "Local inativado com sucesso!";
+	
+	private String pathBotaoSalvar = "/html/body/app-root/app-container/main/div/app-local/div[1]/div/div/form/div[2]/button[2]";
+	private String pathBotaoNovo = "/html/body/app-root/app-container/main/div/app-local/form/div/div[3]/div/button[3]";
+	private String pathBotaoLocais = "//html/body/app-root/app-header/header/div/div/ul[1]/li[4]";
+	
+	private String pathFilter = "/html/body/app-root/app-container/main/div/app-local/form/div/div[2]/div/input";
+	private String pathFilterButton = "/html/body/app-root/app-container/main/div/app-local/form/div/div[3]/div/button[2]";
+	private String pathPrimeiroElementoFiltrado = "/html/body/app-root/app-container/main/div/app-local/div[2]/table/tbody/tr[1]/td[2]";
+	private String pathSegundoElementoFiltradoEdit = "/html/body/app-root/app-container/main/div/app-local/div[2]/table/tbody/tr[2]/td[3]";
+	private String pathPrimeiroElementoFiltradoEdit = "/html/body/app-root/app-container/main/div/app-local/div[2]/table/tbody/tr[1]/td[3]";
+	private String pathPrimeiroElementoFiltradoDelete = "/html/body/app-root/app-container/main/div/app-local/div[2]/table/tbody/tr[1]/td[4]";
+	private String pathAcceptDeleteModal = "/html/body/app-root/app-container/main/div/app-local/app-confirm-modal/div/div/div/div[3]/button[2]";
+	
+	private DSL dsl = new DSL();
+	
+	public void setNome (String descricao) {
+		dsl.escrever("nome", descricao);
+	}
+	
+	//Acessar Página
+	
+	public void irParaMarcas() {
+		dsl.clicarBotao(pathBotaoLocais);
+	}
+
+	//Botões
+	
+	public void salvar() {
+		dsl.clicarBotao(pathBotaoSalvar);
+	}
+	
+	public void novo() {
+		dsl.clicarBotao(pathBotaoNovo);
+	}
+	
+	public void editarDuplicado() {
+		dsl.clicarBotao(pathSegundoElementoFiltradoEdit);
+	}
+	
+	public void editar(String nome) {
+		this.filtrarPorNome(nome);
+		dsl.clicarBotao(pathPrimeiroElementoFiltradoEdit);
+	}
+	
+	public void excluir(String nome) {
+		this.filtrarPorNome(nome);
+		dsl.clicarBotao(pathPrimeiroElementoFiltradoDelete);
+		dsl.clicarBotao(pathAcceptDeleteModal);
+	}
+
+	//Mensagem
+	
+	public String getMsg() {
+		return dsl.obterTexto(By.xpath(pathMensagem));
+	}
+	
+	public String getMsgEditadoSucesso() {
+		return dsl.obterTexto(By.xpath(pathMensagemEditadoSucesso));
+	}	
+	
+	public String msgDescricaoDuplicada() {
+		return msgDescricaoDuplicada;
+	}
+	
+	public String msgSucesso() {
+		return msgSucesso;
+	}
+	
+	public String msgEditadoSucesso() {
+		return msgEditadoSucesso;
+	}
+	
+	//Get
+	
+	public String getNomeCadastrado() {
+		return dsl.obterTexto(By.xpath(pathPrimeiroElementoFiltrado));
+	}
+	
+	//Filtro
+	
+	public void filtrarPorNome(String nomeItem) {
+		dsl.escrever(By.xpath(pathFilter), nomeItem);
+		dsl.clicarBotao(pathFilterButton);
+	}
+	
+
+	public void reload(){
+		dsl.reload();
+	}
+	
 }
--- src/main/java/br/edu/ifrs/TesteLocais.java
+++ src/main/java/br/edu/ifrs/TesteLocais.java
@@ -1,5 +1,118 @@
 package br.edu.ifrs;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+import net.datafaker.Faker;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TesteLocais {
+	
+	private static LocaisPage page = new LocaisPage();
+	public static Faker faker = new Faker();
+	public static String nomeDuplicado = faker.lorem().characters(10, 13) + "_BASE";
+	
+	@Parameter
+	public String nome;
+	@Parameter(value=1)
+	public String msgSucesso;
+	@Parameter(value=2)
+	public String msgNomeDuplicado;
+	@Parameter(value=3)
+	public String msgEditadoSucesso;
+
+	@BeforeClass
+	public static void setup() {
+		 Login.login("eduardo.chassot@aluno.feliz.ifrs.edu.br", "ratones");
+		
+	}
+	
+	@Before
+	public void logar(){
+		page.reload();
+		page.irParaMarcas();
+	}
+	
+	
+	
+	@Parameters
+	public static Collection<Object[]> getCollection() {
+		String nomeGerado = faker.commerce().productName();
+		return Arrays.asList(new Object[][] {
+			{nomeGerado, page.msgSucesso(), page.msgDescricaoDuplicada(), page.msgEditadoSucesso()},
+		});
+	}
+	
+	@Test
+	public void t01_deveCastrarNovoLocal() throws IOException, InterruptedException {
+		page.novo();
+		page.setNome(nome);
+		page.salvar();	
+		System.out.println("mensagem: " + page.getMsg());
+		System.out.println("Nome: " + nome);
+		Assert.assertEquals(msgSucesso, page.getMsg());
+	}
+	
+	@Test
+	public void t02_validaCadastroLocal() throws IOException, InterruptedException {
+		page.filtrarPorNome(nome);
+		Assert.assertEquals(page.getNomeCadastrado(), nome);
+	}
+	
+	@Test
+	public void t03_deveCadastrarComNomeDuplicado() throws IOException {
+		page.novo();
+		page.setNome(nome);
+		page.salvar();
+		Assert.assertEquals(msgNomeDuplicado, page.getMsg());
+	}
+	
+	@Test
+	public void t04_deveEditarLocalDuplicando() throws IOException {
+
+		page.editarDuplicado();
+		page.setNome(nome);
+		page.salvar();	
+		System.out.println("t04: " + page.getMsg());
+		Assert.assertEquals(msgNomeDuplicado, page.getMsg());
+
+	}
+	
+	@Test
+	public void t05_deveEditarLocal() throws IOException {
+
+		page.editar(nome);
+		page.setNome(nome + "EDIT");
+		page.salvar();	
+		System.out.println("mensagemEditadoSucesso: " + page.getMsg());
+		Assert.assertEquals(msgEditadoSucesso, page.getMsg());
+	}
+	
+	@Test
+	public void t06_deveExcluirlocal() throws IOException {
+		page.excluir(nome + "EDIT");
+		Assert.assertEquals(page.msgDeleteSucesso, page.getMsg());
+	}
+	
+	
+	@AfterClass
+	public static void fechar() throws InterruptedException{
+		DriverFactory.killDriver();
+	}
     
+	
+	
 }
