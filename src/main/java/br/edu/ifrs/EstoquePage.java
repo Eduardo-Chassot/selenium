763a966e7eb85bd3db61d3ed5cba291aedf3a375
NIE9 <o.thiagonienov@gmail.com>
2025-07-07 18:15:03 -0300

    Estoque
    Estoque iniciado

--- src/main/java/br/edu/ifrs/EstoquePage.java
+++ src/main/java/br/edu/ifrs/EstoquePage.java
@@ -3,27 +3,37 @@ package br.edu.ifrs;
 import org.openqa.selenium.By;
 
 public class EstoquePage {
-    private String pathMensagemDescricaoDuplicada = "/html/body/app-root/app-container/main/div/app-unidade-medida/p-toast/div/p-toastitem/div/div/div/div[1]";
-	private String pathMensagemSucesso = "/html/body/app-root/app-container/main/div/app-unidade-medida/p-toast/div/p-toastitem/div/div/div/div[2]";
-	private String pathMensagemEditadoSucesso = "/html/body/app-root/app-container/main/div/app-unidade-medida/p-toast/div/p-toastitem";
-	private String pathBotaoSalvar = "/html/body/app-root/app-container/main/div/app-unidade-medida/div[1]/div/div/form/div[2]/button[2]";
-	private String pathBotaoNovo = "/html/body/app-root/app-container/main/div/app-unidade-medida/form/div/div[3]/div/button[3]";
-	private String pathBotaoUnidadesMedida = "/html/body/app-root/app-header/header/div/div/ul[1]/li[3]/a";
-	private String pathUnidadeCadastrada = "/html/body/app-root/app-container/main/div/app-unidade-medida/div[2]/table/tbody/tr[13]/td[3]/i";
-	
-	private String msgSucesso = "Medida cadastrada com sucesso!";
-	private String msgDescricaoDuplicada = "Já existe uma Unidade de Medida com a mesma descrição!";
-	private String msgEditadoSucesso = "Medida editada com sucesso";
+	//private String pathMensagem = "/html/body/app-root/app-container/main/div/app-marca/p-toast/div/p-toastitem/div/div/div/div[2]";
+	//private String pathMensagemEditadoSucesso = "/html/body/app-root/app-container/main/div/app-marca/p-toast/div/p-toastitem";
+	//private String pathBotaoSalvar = "/html/body/app-root/app-container/main/div/app-marca/div[1]/div/div/form/div[2]/button[2]";
+	private String pathBotaoNovo = "/html/body/app-root/app-container/main/div/app-movimentacao/form/div/div[5]/div/button[3]";
+	private String pathBotaoEstoque = "/html/body/app-root/app-header/header/div/div/ul[1]/li[7]";
+	//private String pathFilter = "/html/body/app-root/app-container/main/div/app-marca/form/div/div[2]/div/input";
+	//private String pathFilterButton = "/html/body/app-root/app-container/main/div/app-marca/form/div/div[3]/div/button[2]";
+	//private String pathPrimeiroElementoFiltrado = "/html/body/app-root/app-container/main/div/app-marca/div[2]/table/tbody/tr[1]/td[2]";
+	//private String pathPrimeiroElementoFiltradoEdit = "/html/body/app-root/app-container/main/div/app-marca/div[2]/table/tbody/tr[1]/td[3]";
+	//private String pathPrimeiroElementoFiltradoDelete = "/html/body/app-root/app-container/main/div/app-marca/div[2]/table/tbody/tr[1]/td[4]";
+	//private String pathAcceptDeleteModal = "/html/body/app-root/app-container/main/div/app-marca/app-confirm-modal/div/div/div/div[3]/button[2]";
+	//private String msgSucesso = "Marca cadastrada com sucesso!";
+	//private String msgDescricaoDuplicada = "Já existe uma Marca com o mesmo nome!";
+	//private String msgEditadoSucesso = "Marca editada com sucesso!";
+	public String msgDeleteSucesso = "Marca inativada com sucesso!";
 	
 	private DSL dsl = new DSL();
 	
-	public void setDescricao (String descricao) {
-		dsl.escrever("descricao", descricao);
+	public void irParaEstoque() {
+		dsl.clicarBotao(pathBotaoEstoque);
+	}
+	
+	//Campos
+	
+	
+	public void getItem (String descricao) {
+		dsl.obterValueCombo("cadastro_elemento");
+		dsl.selecionarComboByValue("cadastro_elemento", String valor);
 	}
 
-	public String getPathMensagemDescricaoDuplicada() {
-		return pathMensagemDescricaoDuplicada;
-	}
+	//Botões
 	
 	public void salvar() {
 		dsl.clicarBotao(pathBotaoSalvar);
@@ -33,13 +43,9 @@ public class EstoquePage {
 		dsl.clicarBotao(pathBotaoNovo);
 	}
 
-	public String getMsgSucesso() {
-		return dsl.obterTexto(By.xpath(pathMensagemSucesso));
+	public String getMsg() {
+		return dsl.obterTexto(By.xpath(pathMensagem));
 	}
-	
-	public String getMsgDescricaoDuplicada() {
-		return dsl.obterTexto(By.xpath(pathMensagemDescricaoDuplicada));
-	}	
 	
 	public String getMsgEditadoSucesso() {
 		return dsl.obterTexto(By.xpath(pathMensagemEditadoSucesso));
@@ -57,20 +63,41 @@ public class EstoquePage {
 		return msgEditadoSucesso;
 	}
 	
-	public void irParaUnidadesMedida() {
+	public void irParaMarcas() {
 		dsl.clicarBotao(pathBotaoUnidadesMedida);
 	}
 	
-	public void editar() {
-		dsl.clicarBotao(pathUnidadeCadastrada);	
+	public void editar(String nome) {
+		this.filtrarPorNome(nome);
+		dsl.clicarBotao(pathPrimeiroElementoFiltradoEdit);
 	}
 	
-	public String setDescricaoCadastrada() {
-		return dsl.obterTexto(By.xpath("/html/body/app-root/app-container/main/div/app-unidade-medida/div[2]/table/tbody/tr[13]/td[2]"));
+	public void excluir(String nome) {
+		this.filtrarPorNome(nome);
+		dsl.clicarBotao(pathPrimeiroElementoFiltradoDelete);
+		dsl.clicarBotao(pathAcceptDeleteModal);
 	}
 	
-	public String getDescricaoCadastrada() {
-		return setDescricaoCadastrada();
+	public String getNomeCadastrado() {
+		return dsl.obterTexto(By.xpath(pathPrimeiroElementoFiltrado));
+	}
+	
+	public void filtrarPorNome(String nomeItem) {
+		dsl.escrever(By.xpath(pathFilter), nomeItem);
+		dsl.clicarBotao(pathFilterButton);
+	}
+	
+
+	public void reload(){
+		dsl.reload();
 	}
 
+	public String getMsgDeleteSucesso() {
+		return msgDeleteSucesso;
+	}
+
+	public void setMsgDeleteSucesso(String msgDeleteSucesso) {
+		this.msgDeleteSucesso = msgDeleteSucesso;
+	}
+	
 }
